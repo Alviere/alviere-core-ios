773be19ea9c9fc0623fdcfe086fb954c480eced5
Continuous Integration <mobile-ci@mezu.com>
2022-10-31 18:40:44 +0000

    0.9.19

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AlCore",
-            url: "https://github.com/Alviere/alviere-core-ios/releases/download/0.9.18/AlCore.xcframework.zip",
-            checksum: "b2cf6405dbf05b09bc2139af7311664dda42aa3481898b8e07d14a36c2ff5e4a"
+            url: "https://github.com/Alviere/alviere-core-ios/releases/download/0.9.19/AlCore.xcframework.zip",
+            checksum: "cb90ba918183b9735bac4c52e66b0dfd96920948574d468177774d97313580b5"
         )
     ]
 )
